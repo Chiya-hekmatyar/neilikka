45ded259305351d187f2628c7db23d749c47a5b8
Chiya-hekmatyar <chiya.hekmatyar@edu.omnia.fi>
2024-09-26 11:55:55 +0300

    Update etusivu.php

--- etusivu.php
+++ etusivu.php
@@ -7,12 +7,11 @@ include 'head.php';
 <body>
 <?php include 'navigointi.html'; ?>  
 <div class="content">
-  <h1>Puutarhaliike Neilikka</h1>
-  <p>Tervetuloa Puutarhaliike Neilikan kotisivuille! Meiltä löydät sekä sisä- että ulkokasvit ja kaiken tarvitsemasi kasvien hoitoon.</p>
-  <h1>Puutarhauutisia</h1>
-    <p>2.1.2016 Hyvää uutta vuotta! Uuden vuoden kunniaksi myymälöissämme upeita tarjouksia.</p>
-    <p>14.12.2015 Joulukukat edullisesti meiltä. Myymälöissämme myös kattava ja edullinen valikoima joulukuusia.</p>
-    <p>1.9.2015 Nyt on hyvä aika aloittaa puutarhan valmistelu talven lepokautta varten. Meiltä löydät kaikki työkalut ja tarvikkeet.</p>
+  <h1>Koulutusliike Talli</h1>
+  <p>Tervetuloa Koulutusliike Talli kotisivuille! Täältä löydät tietoa meiden koulutuksista ja rekrytointiohjelmistamme.</p>
+  <h1>Koulutukset</h1>
+    <p>Tarjoamme paljon erillaisia koulutuksia kaiken tasoisille oppillaille tai halukkaille</p>
+    
 </div>
 <?php include 'footer.html'; ?>
 </body>
